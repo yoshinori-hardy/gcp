1effefd5b67ba5e5816e31d41f7795f32640e8bc
Yoshinori Hardy <yoshinori.v.hardy@accenture.com>
2018-04-11 12:28:40 +0000

    first attempts at iam

--- modules/network/iam.tf
+++ modules/network/iam.tf
@@ -3,17 +3,10 @@ resource "google_service_account" "nat-service-account" {
   display_name = "nat-service-account"
 }
 
-resource "google_project_iam_policy" "nat-iam" {
-  depends_on  = ["google_service_account.nat-service-account"]
-  policy_data = "${data.google_iam_policy.nat.policy_data}"
-}
+resource "google_project_iam_binding" "nat" {
+  role    = "projects/admin-200808/roles/CustomNATIAMRole"
 
-data "google_iam_policy" "nat" {
-  binding {
-    role = "roles/CustomNATIAMRole"
-
-    members = [
-      "serviceAccount:${google_service_account.nat-service-account.email}",
-    ]
-  }
+  members = [
+    "serviceAccount:${google_service_account.nat-service-account.email}",
+  ]
 }
