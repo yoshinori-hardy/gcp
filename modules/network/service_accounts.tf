130e4ec4f078f55af0445ac2a72071cb2bf73bdf
Yoshinori Hardy <yoshinori.v.hardy@accenture.com>
2018-04-11 16:23:41 +0000

    end of the day

--- modules/network/service_accounts.tf
+++ modules/network/service_accounts.tf
@@ -10,3 +10,16 @@ resource "google_project_iam_binding" "nat" {
     "serviceAccount:${google_service_account.nat-service-account.email}",
   ]
 }
+
+resource "google_service_account" "vpn-service-account" {
+  account_id   = "vpn-service-account"
+  display_name = "vpn-service-account"
+}
+
+resource "google_project_iam_binding" "vpn" {
+  role    = "projects/peaceful-web-200808/roles/object_ro"
+
+  members = [
+    "serviceAccount:${google_service_account.vpn-service-account.email}",
+  ]
+}
---
+++ modules/network/vpn.tf
@@ -0,0 +1,70 @@
+// Obtain EIP for the NAT Instance
+//resource "google_compute_address" "vpn-ip" {
+//  name = "vpn-ip-1"
+//  region = "${var.region}"
+//  address_type = "EXTERNAL"
+//}
+
+resource "google_compute_region_instance_group_manager" "vpn-servers" {
+  name = "vpn-servers-igm"
+  base_instance_name = "vpn-server"
+  instance_template  = "${google_compute_instance_template.vpn_instance_template.self_link}"
+  region             = "${var.region}"
+  target_size  = 1
+}
+
+data "template_file" "vpn-start-script" {
+  template = <<EOF
+#!/bin/bash -eax
+sudo service iptables start
+sudo chkconfig iptables on
+sysctl -w net.ipv4.ip_forward=1
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+EOF
+  }
+
+resource "google_compute_instance_template" "vpn_instance_template" {
+  name        = "vpn-instance-template"
+  depends_on = ["google_service_account.vpn-service-account"]
+  description = "This template is used to build VPN Instances"
+
+// add project ID to the tags?
+  tags = ["nat", "fw-ssh", "fw-vpn"]
+
+  labels = {
+    role = "vpn-services"
+  }
+
+  instance_description = "Built using TF instance template"
+  machine_type         = "${var.machine_type}"
+  depends_on = ["google_service_account.vpn-service-account"]
+  can_ip_forward       = false
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+  }
+
+  disk {
+    source_image = "${var.source_image}"
+    auto_delete  = true
+    boot         = true
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.sub-dmz.self_link}"
+    access_config = {
+      # nat_ip = "${google_compute_address.nat-ip.address}"
+    }
+  }
+
+  metadata {
+    startup-script = "${data.template_file.vpn-start-script.rendered}"
+  }
+
+  service_account {
+    email = "${google_service_account.vpn-service-account.email}"
+    scopes = ["storage-ro"]
+  }
+}
+
