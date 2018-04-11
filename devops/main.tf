5c0d07ed75bd0145f540a9bee138c6068e0beaea
Yoshinori Hardy <yoshinori.v.hardy@accenture.com>
2018-04-11 13:54:07 +0000

    build out

--- devops/main.tf
+++ devops/main.tf
@@ -8,10 +8,10 @@ module "build_the_vpc" {
   source_image = "centos-6-v20180314"
   machine_type = "g1-small"
   region       = "europe-west2"
-  subnet_dmz     = "10.1.1.0/24"
-  subnet_app     = "10.1.2.0/24"
+  subnet_dmz   = "10.1.1.0/24"
+  subnet_app   = "10.1.2.0/24"
 }
 
 module "storage" {
-  source = "../modules/storage"
+  source       = "../modules/storage"
 }
