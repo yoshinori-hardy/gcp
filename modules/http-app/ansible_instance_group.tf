f76a227efbb7b167a88712c87f3069dfb17163d0
Yoshinori Hardy <yoshinori.v.hardy@accenture.com>
2018-04-17 16:14:47 +0000

    today's work

--- modules/http-app/ansible_instance_group.tf
+++
@@ -1,7 +0,0 @@
-resource "google_compute_region_instance_group_manager" "app-servers" {
-  region             = "${var.region}"
-  name               = "${var.name}-igm"
-  base_instance_name = "${var.name}-server"
-  instance_template  = "${google_compute_instance_template.app_instance_template.self_link}"
-  target_size        = "${var.target_size}"
-}
